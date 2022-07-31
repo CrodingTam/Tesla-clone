da9eaab535aad120c34b5da3116ce7b37ce9d0d3
SzTamass <sz.tamas@camelcoding.com>
2022-07-31 14:44:15 +0300

    Sections for the tesla clone, other updates on mod

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//rfce -react functional export component
+function Header(){
+    return(
+        <div>
+            header
+        </div>
+    )
+}
+
+export default Header;
